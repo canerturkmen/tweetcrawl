b8808be0a34ef264465d5b4d6fa59080a869bf38
Caner Turkmen <caner@exa-tech.com>
2013-06-30 23:46:15 +0300

    built logging and error handling for base crawler

--- crawler/base.py
+++ crawler/base.py
@@ -1,29 +1,38 @@
-from db.backend import DBConnection
-import logging
 from datetime import datetime
 from twapi.api import TwCrawlAPI
-import time
+from twitter import TwitterError
+from db.backend import DBConnection
+import time, sys, logging, traceback
+
 
 class BaseCrawler:
 
-    _logger = None
-
     def __init__(self):
-        logging.basicConfig(filename="logs/twcrawl_log_%s.log" % datetime.now().strftime("%Y%m%d_%H%M"),
-                            format='%(asctime)-15s %(message)s',
+        logging.basicConfig(#filename="logs/twcrawl_log_%s.log" % datetime.now().strftime("%Y%m%d_%H%M"),
+                            filename="logs/twcrawl_log_.log",
+                            format='%(asctime)-15s %(levelname)s : %(message)s',
                             level=logging.INFO)
         self._logger = logging.getLogger()
-#        self._db = DBConnection
+        self._db = DBConnection
         self._api = TwCrawlAPI()
 
     def run(self):
         self._logger.info("Initializing run for BaseCrawler")
+        try:
+            followers = self._api.getUserFollowers("canerturkmen")
+            for i in followers:
+                self._db.persistUser(i)
+                print str(i.AsDict())
+                break
 
-        followers = self._api.getUserFollowers("canerturkmen")
-        for i in followers:
-            # DBConnection.persistUser(i)
-            print i.AsDict
-            break
+        except TwitterError as e:
+            self._logger.error("TwitterError encountered.")
+            self._logger.error("Print StackTrace : \n %s" % traceback.format_exc())
+
+        except:
+            self._logger.critical("Error encountered.")
+            sys.exit(status=0)
+
 
         self._logger.info("Sleeping for 1 minute")
         time.sleep(60) # sleep for 60 seconds
